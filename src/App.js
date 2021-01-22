3686dbb78c8d5480d505eee89c5e62895302759f
cpkuo <cpkuo@cht.com.tw>
2021-01-22 14:03:02 +0800

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
-import { Button } from '@material-ui/core';
 
 const App = () => {
     const [state, setState] = useState("CLICK ME");
 
     return (
         <div>
-            <Button onClick={() => setState("HELLO")}>{state}</Button>
+            <button onClick={() => setState("HELLO")}>{state}</button>
         </div>
     
     );
